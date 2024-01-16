3e41b44bb0fbd6a71fea901b9d68ffb292f02386
ThomasHuynh <Thomas_TTH@live.com>
2024-01-15 22:33:56 -0500

    Implemented API call and added image search functionality

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,27 +4,61 @@ import defaultImg from "../assets/default_image.svg";
 type Props = {};
 
 const Home = (props: Props) => {
-  const [imageUrl, setImgUrl] = useState<boolean>("/");
+  const [imgUrl, setImgUrl] = useState<string>("/");
   let inputRef = useRef(null);
 
-  return (
-    <header className="mx-auto flex h-screen w-full max-w-[768px] flex-col items-center justify-center">
-      <h1 className="py-8 text-4xl font-bold">
-        Ai Image <span className="text-[#DE1B89]">Generator</span>
-      </h1>
-      <div>
-        <img src={defaultImg} alt="Default image" />
-      </div>
+  const generateImg = async () => {
+    // if (inputRef.current.value === "") {
+    //   return 0;
+    // }
 
-      <div className="my-6 flex h-[60px] w-1/2 items-center justify-between rounded-full bg-[#1F3540] pl-6 pr-2">
-        <input
-          type="text"
-          placeholder="Describe what you want to see"
-          className="h-1/2 w-2/3 bg-[#1F3540] text-white outline-none"
-        />
-        <button className="h-[45px] rounded-full bg-[#DE1B89] px-4 py-2 text-sm text-white hover:opacity-80">
-          Generate
-        </button>
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer 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",
+          "User-Agent": "Chrome",
+        },
+        body: JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n: 1,
+          size: "512x512",
+        }),
+      },
+    );
+
+    const data = await response.json();
+    console.log(data);
+    setImgUrl(data.data[0].url);
+  };
+
+  return (
+    <header className="flex h-screen w-full bg-[#0f1b21]">
+      <div className="mx-auto flex max-w-[768px] flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-white">
+          Ai Image <span className="text-[#DE1B89]">Generator</span>
+        </h1>
+        <div className="py-8">
+          <img src={imgUrl === "/" ? defaultImg : imgUrl} alt="/" />
+        </div>
+
+        <div className="mx-auto flex h-[65px] w-3/4 items-center justify-between rounded-full bg-[#1F3540] p-2">
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Describe what you want to see"
+            className="mx-4 h-1/2 w-full bg-[#1F3540] text-white outline-none"
+          />
+          <button
+            onClick={generateImg}
+            className="h-full rounded-full bg-[#DE1B89] px-4 py-2 text-sm text-white hover:opacity-80"
+          >
+            Generate
+          </button>
+        </div>
       </div>
     </header>
   );
